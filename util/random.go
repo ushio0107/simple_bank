822242b32c42f371df752ca2d19dd5317c1c244f
ushio0107 <ushio000107@gmail.com>
2023-10-14 22:06:59 +0800

    add random generator

--- util/random.go
+++ util/random.go
@@ -1,1 +1,30 @@
 package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+var r *rand.Rand
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+func RandomInt(min, max int64) int64 {
+	return min + r.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[r.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}

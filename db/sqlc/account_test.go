e6c53707e0d453c5e427b27f02b69ae6324e0c23
ushio0107 <ushio000107@gmail.com>
2023-10-14 21:49:47 +0800

    add unit test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -17,4 +17,11 @@ func TestCreateAccount(t *testing.T) {
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 }

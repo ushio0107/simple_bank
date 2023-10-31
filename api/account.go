fda485565aa8174f7429ac157b8fcb8073a80967
ushio0107 <ushio000107@gmail.com>
2023-10-31 20:33:01 +0800

    fea: add routes updateAc and deleteAc

--- api/account.go
+++ api/account.go
@@ -84,3 +84,61 @@ func (s *Server) listAccounts(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, list)
 }
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (s *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := s.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Specific account id isn't be found, not existed.
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		// TODO: Some accounts may have transfer record in entry relations, can't be delete.
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, "DELETE")
+}
+
+type updateAccountRequest struct {
+	ID      int64 `json:"id" binding:"required,min=1"`
+	Balance int64 `json:"balance" binding:"required,min=1"`
+}
+
+func (s *Server) updateAccount(ctx *gin.Context) {
+	var req updateAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.UpdateAccountParams{
+		ID:      req.ID,
+		Balance: req.Balance,
+	}
+	ac, err := s.store.UpdateAccount(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Specific account id isn't be found, not existed.
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, ac)
+}
--- api/server.go
+++ api/server.go
@@ -18,6 +18,8 @@ func NewServer(store *db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.PUT("/accounts", server.updateAccount)
 
 	server.router = router
 	return server

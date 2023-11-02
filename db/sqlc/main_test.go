5a3346178c53a0dfb44d398b366f959c0488f06f
ushio0107 <ushio000107@gmail.com>
2023-11-02 23:24:49 +0800

    fea: add mock db

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"simple_bank/util"
 	"testing"
 
 	_ "github.com/lib/pq"
@@ -15,14 +16,12 @@ var (
 	testDB      *sql.DB
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:123456@127.0.0.1:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Failed to load config: ", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -5,25 +5,24 @@ import (
 	"log"
 	"simple_bank/api"
 	db "simple_bank/db/sqlc"
+	"simple_bank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:123456@127.0.0.1:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Failed to load config: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Failed to connect to DB: ", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Failed to start the server: ", err)
 	}

59f3e9b226a67f0d1eec49d94333a336e6b30749
ushio0107 <ushio000107@gmail.com>
2023-11-06 21:26:18 +0800

    fea: add password hash function

---
+++ util/password_test.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(8)
+
+	hashedPassword1, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword1)
+
+	err = CheckPassword(password, hashedPassword1)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, hashedPassword1)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	// To ensure the same password is hashed differently each time.
+	hashedPassword2, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEqual(t, hashedPassword1, hashedPassword2)
+
+}

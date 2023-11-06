315b84538fe0f8d8b6e63eb8d8656ef0e003f702
ushio0107 <ushio000107@gmail.com>
2023-11-06 20:54:37 +0800

    fix: interface unimplemented

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -8,6 +8,7 @@ import (
 	"context"
 )
 
+// When we add a new interface, we need to add a new method in the mock store.
 type Querier interface {
 	AddAccountBalance(ctx context.Context, arg AddAccountBalanceParams) (Account, error)
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)

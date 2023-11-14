96357cfe1abd09a164e046dfc893c18c4c051ad8
ushio0107 <ushio000107@gmail.com>
2023-11-15 00:34:08 +0800

    fix: list accounts

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -80,19 +80,22 @@ func TestDeleteAccount(t *testing.T) {
 }
 
 func TestListAccount(t *testing.T) {
+	var ac Account
 	for i := 0; i < 10; i++ {
-		createRandomAccount(t)
+		ac = createRandomAccount(t)
 	}
 
 	arg := ListAccountsParams{
+		Owner:  ac.Owner,
 		Limit:  5,
-		Offset: 5,
+		Offset: 0,
 	}
 	accounts, err := testQueries.ListAccounts(context.Background(), arg)
 	require.NoError(t, err)
-	require.Len(t, accounts, 5)
+	require.NotEmpty(t, accounts)
 
 	for _, account := range accounts {
 		require.NotEmpty(t, account)
+		require.Equal(t, ac.Owner, account.Owner)
 	}
 }
